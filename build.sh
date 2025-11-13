afd238b1f9fc96213f5f4a21555811bcbdd52630
musmana <shinymusmana@gmail.com>
2025-11-14 03:22:14 +0530

    fix build script

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Run migrations
 python manage.py migrate --noinput
-
-# Collect static files
-python manage
+python manage.py collectstatic --noinput
--- myshop/settings.py
+++ myshop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,12 +77,20 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Default (Local) — use SQLite
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# Render (Production) — use PostgreSQL
+if os.environ.get('RENDER'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
 
 
 # Password validation
